d603f6e74e8eaf6e9b1ad873fbc2bade4d8f684e
Rodrigo Azevedo <rodrigo@rwba.dev>
2023-01-30 22:20:06 -0300

    Fetch Many by EndId with join

---
+++ test/integration/infrastructure/adapters/outbound/database/order/fetchable-order.adapter.spec.ts
@@ -0,0 +1,22 @@
+
+import { FetchableOrderDbAdapter } from '../../../../../../../src/infrastructure/adapters/outbound/database/order/fetchable-order.adapter';
+import { SettingsAdapter } from '../../../../../../../src/infrastructure/adapters/outbound/environment/settings.adapter';
+import { Settings } from '../../../../../../../src/domain/common/settings';
+import { KnexPostgresDatabase } from '../../../../../../../src/infrastructure/adapters/outbound/database/knex-postgres.db';
+
+describe('FetchOrderAdapter', () => {
+  it('fetchManyByEndId', async () => {
+    const settings: Settings = SettingsAdapter.getInstance().getSettings();
+    const knexPostgresDb = KnexPostgresDatabase.getInstance(settings);
+
+    const fetchOrderAdapter = FetchableOrderDbAdapter.getInstance(knexPostgresDb);
+
+    const result = await fetchOrderAdapter.fetchManyByEndId([
+      '6TUZA6EKXPP3GR6M092AAGCFA',
+      '4Z8OFQEMP7WHG1FX0XBGDM39X',
+      'ML879928GPEO91KHDZHGD7YW',
+    ]);
+
+    console.log(result);
+  });
+});

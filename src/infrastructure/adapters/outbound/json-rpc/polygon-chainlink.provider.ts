c0a13a67436355587b5b5e89f12a7d9aab4d36bd
Pedro Savelis <savelis.pedro@gmail.com>
2023-05-13 12:26:15 -0300

    feat(qa): add contracts on test network (mumbai, goerli)

--- src/infrastructure/adapters/outbound/json-rpc/polygon-chainlink.provider.ts
+++ src/infrastructure/adapters/outbound/json-rpc/polygon-chainlink.provider.ts
@@ -22,14 +22,18 @@ export class PolygonChainlinkProvider implements IChainlinkProtocolProvider {
           settings.blockchain.polygon.providerEndpoint,
         );
 
-      const maticUsdAddress =
-        process.env.NODE_ENV === 'production'
-          ? '0xab594600376ec9fd91f8e885dadf0ce036862de0' // polygonmainnet: https://data.chain.link/polygon/mainnet/crypto-usd/matic-usd
-          : '0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada'; // mumbai: https://docs.chain.link/data-feeds/price-feeds/addresses/?network=polygon
+      const isProd = process.env.NODE_ENV === 'production';
 
-      const maticEthAddress = '0x327e23a4855b6f663a28c5161541d69af8973302'; //https://data.chain.link/polygon/mainnet/crypto-eth/matic-eth
+      const maticUsdAddress = isProd
+        ? '0xab594600376ec9fd91f8e885dadf0ce036862de0' // polygonmainnet: https://data.chain.link/polygon/mainnet/crypto-usd/matic-usd
+        : '0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada'; // mumbai: https://docs.chain.link/data-feeds/price-feeds/addresses/?network=polygon
+
+      const maticEthAddress = isProd
+        ? '0x327e23a4855b6f663a28c5161541d69af8973302' //https://data.chain.link/polygon/mainnet/crypto-eth/matic-eth
+        : '0x8106d642b15a27bfecfc71f2e94a21159dca2462'; // mumbai: AggregatorV3Mock.sol
 
       PolygonChainlinkProvider.dataFeed = {
+        //
         ['matic-usd']: new ethers.Contract(
           maticUsdAddress,
           aggregatorV3InterfaceABI,

8534f2469c95400f19f2b32d8e98f80379f04f8b
Pedro Savelis <savelis.pedro@gmail.com>
2023-05-24 11:59:34 -0300

    feat(tokenomics-dto): wip

--- src/domain/statistics/dtos/tokenomics.dto.ts
+++ src/domain/statistics/dtos/tokenomics.dto.ts
@@ -1,18 +1,46 @@
+import { Chain } from '../../common/entities/chain.entity';
 import { NetworkType } from '../../common/enums/network-type.enum';
 
 export interface TokenomicsDto {
   totalSupply: number;
   circulatingSupply: number;
-  holders: number;
-  lockedTokens: number;
-  marketCap: number;
-  treasuryMarketValue: number;
+  holders: {
+    count: number;
+    totalAmount: number;
+  };
+  lockedTokens: {
+    totalAmount: number;
+    preSale: number;
+    sale: number;
+  };
+  // TVL (total locked value)
+  totalLockedValue: {
+    usd: number;
+    brl: number;
+    matic: number;
+    eth: number;
+  };
+  // marketCap := circulatingSupply * price
+  marketCap: {
+    usd: number;
+    brl: number;
+    matic: number;
+    eth: number;
+  };
+  // TVM (treasury market value) => to be defined
+  treasuryMarketValue: {
+    usd: number;
+    brl: number;
+    matic: number;
+    eth: number;
+  };
   price: {
     usd: number;
     brl: number;
     matic: number;
     eth: number;
   };
+  networks: Array<Chain>;
   contracts: Record<
     NetworkType,
     {
